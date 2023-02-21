ec08d0c1834a7ed650a5c910329939b59d1332ae
Yuri Pilov <sharikov@gmail.com>
2023-02-21 15:41:15 +0300

    6 sprint - final project - attempt N11

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -73,7 +73,7 @@ class PostPagesTests(TestCase):
                 'posts:post_detail', kwargs={'post_id': f'{self.post.id}'}
             ): 'posts/post_detail.html',
             reverse(
-                'posts:edit', kwargs={'post_id': f'{self.post.id}'}
+                'posts:post_edit', kwargs={'post_id': f'{self.post.id}'}
             ): 'posts/create_post.html',
         }
         for reverse_name, template in templates_url_address.items():
